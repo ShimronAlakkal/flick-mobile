7cfb68046b5d916b674ca861e45378b9bc07a2a3
ShimronAlakkal <shimron.alakkal1804@gmail.com>
2023-02-15 21:26:54 -0500

    pubspeck update, firebase packages added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flick/common/display_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  await SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitUp],
+  );
+
   runApp(
     const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: DisplayRouter(),
     ),
   );
