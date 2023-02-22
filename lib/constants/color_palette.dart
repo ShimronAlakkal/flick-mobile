5d4c2d0f8a6ee848ad5a13fcbdeeb68f8f2b49bc
ShimronAlakkal <shimron.alakkal1804@gmail.com>
2023-02-22 16:16:33 -0500

    new colors added to palette

--- lib/constants/color_palette.dart
+++ lib/constants/color_palette.dart
@@ -21,4 +21,31 @@ class ColorPalette {
 
   // proceed green
   final Color liveGreen = const Color(0xffa8e718);
+
+  // the color used in icons when inactive
+  final Color inactiveIconGrey = const Color(0xff878484);
+
+  // grey used in highlighted containers
+  final Color containerGrey = const Color(0xffEDE8E8);
+
+  // The color for secondary/not so important text
+  final Color secondaryTextColor = const Color(0xff393939);
+
+  // the tirtiary text color
+  final Color tertiaryTextColor = const Color(0xff878484);
+
+  // the background color
+  final Color backgroundColor = const Color(0xffF8F8F8);
+
+  // the main title color
+  final Color mainTitleColor = const Color(0xff2A2426);
+
+  // container fill grey
+  final Color containerFillGrey = const Color(0xffF3F4F8);
+
+  // touch blue
+  final Color touchBlue = const Color(0xffE2F0F0);
+
+  // The blue used in clickable links in the UI
+  final Color linkBlue = const Color(0xff1976D2);
 }
