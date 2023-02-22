85a14528bda2eb9d9bed45d0980b47f757298da1
ShimronAlakkal <shimron.alakkal1804@gmail.com>
2023-02-22 16:17:04 -0500

    sign in page ui over, su in progress

--- lib/views/auth/sign_in_page.dart
+++ lib/views/auth/sign_in_page.dart
@@ -1,5 +1,5 @@
-import 'package:flick/common/top_lable_text_field.dart';
 import 'package:flick/constants/color_palette.dart';
+import 'package:flick/common/widgets/top_lable_text_field.dart';
 import 'package:flutter/material.dart';
 
 class SignInPage extends StatefulWidget {
@@ -12,26 +12,132 @@ class SignInPage extends StatefulWidget {
 class _SignInPageState extends State<SignInPage> {
   TopLabelTextField topLabelTextFieldInstance = TopLabelTextField();
   TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        children: [
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
 
-          // email field
-          topLabelTextFieldInstance.topLabelTextField(
-            controller: emailController,
-            label: 'Email',
-            hintText: '',
-            keyboardType: TextInputType.emailAddress,
-            obscureText: false,
-            requiredField: true,
-            maxLines: 1,
-            borderRadius: 10,
-            borderColor: ColorPalette().primaryBlack
-          ),
-        ],
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: width * 0.08),
+        child: ListView(
+          children: [
+
+            SizedBox(height: height * 0.04),
+
+            // illustration
+            Center(
+              child: CircleAvatar(
+                backgroundColor: Colors.transparent,
+                foregroundColor: Colors.transparent,
+                radius: width * 0.3,
+                child: Image.asset('assets/illustrations/connect.png'),
+              ),
+            ),
+
+            SizedBox(
+              height: height * 0.035 / 2,
+            ),
+
+            // The small desription on what to do next
+            Text(
+              "We're super excited to have you here!!\nSign up to play, enjoy, and have a wonderful time.",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: ColorPalette().tertiaryTextColor,
+                fontSize: 14,
+              ),
+            ),
+
+            SizedBox(
+              height: height * 0.08,
+            ),
+
+            // email field
+            topLabelTextFieldInstance.topLabelTextField(
+              controller: emailController,
+              label: 'Email',
+              hintText: '',
+              keyboardType: TextInputType.emailAddress,
+              obscureText: false,
+              requiredField: true,
+              maxLines: 1,
+              borderRadius: 23,
+              borderColor: ColorPalette().primaryBlack,
+            ),
+
+            SizedBox(
+              height: height * 0.02,
+            ),
+
+            // password field
+            topLabelTextFieldInstance.topLabelTextField(
+              controller: emailController,
+              label: 'Password',
+              hintText: '',
+              keyboardType: TextInputType.emailAddress,
+              obscureText: false,
+              requiredField: true,
+              maxLines: 1,
+              borderRadius: 23,
+              borderColor: ColorPalette().primaryBlack,
+            ),
+
+            SizedBox(
+              height: height * 0.06,
+            ),
+
+            // sign in Button
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: ColorPalette().primaryBlack,
+                  side: BorderSide(
+                    width: 1,
+                    color: ColorPalette().inactiveIconGrey,
+                  ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  elevation: 0,
+                  padding: const EdgeInsets.all(12)),
+              // sign user in
+              onPressed: () {},
+              child: const Text(
+                'Sign In',
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+            ),
+
+            SizedBox(
+              height: height * 0.06,
+            ),
+
+            // New to flick? sign up
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'New to Flick? ',
+                  style: TextStyle(
+                      fontSize: 15, color: ColorPalette().tertiaryTextColor),
+                ),
+                InkWell(
+                  // Go to sign up page
+                  onTap: () {},
+                  child: Text(
+                    'Sign Up',
+                    style:
+                        TextStyle(fontSize: 15, color: ColorPalette().linkBlue),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
